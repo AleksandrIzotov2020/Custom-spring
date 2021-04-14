607e6e855d045ddc2602536f731fc22a65112ccd
AleksandrIzotov2020 <aleksandr.izotov20@yandex.ru>
2021-04-14 19:55:24 +0300

    first commit

---
+++ src/main/java/org/example/CronaDesinfector.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class CronaDesinfector {
+
+    public void start(Room room){
+
+    }
+
+    private void desinfect(Room room){
+        System.out.println("Проводится дезинфекция!");
+    }
+}
